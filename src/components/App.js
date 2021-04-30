cce3f6369f8847660bd94966e046fee184f42c70
Akshitha-Ajalade <81165727+Akshitha-Ajalade@users.noreply.github.com>
2021-04-30 20:51:58 +0530

    updated code

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,13 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 
 const App = () => {
+  const text = "I am learning React. My life is getting better.";
   return (
-    <div id="main"></div>
-  )
-}
-
+    <div id="main">
+      <p>{text}</p>
+    </div>
+  );
+};
 
 export default App;
