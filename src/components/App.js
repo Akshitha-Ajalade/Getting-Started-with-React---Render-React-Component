b14f8104a2dd47ee4c627d72ee09c792b1613e89
Akshitha-Ajalade <81165727+Akshitha-Ajalade@users.noreply.github.com>
2021-04-30 21:13:15 +0530

    updated code

--- src/components/App.js
+++ src/components/App.js
@@ -2,10 +2,12 @@ import React, { Component, useState } from "react";
 import "../styles/App.css";
 
 const App = () => {
-  const text = "I am learning React. My life is getting better.";
+  const heading = "E-commerse website";
+  const description = "i want to make a website about some pizza and coffee";
   return (
     <div id="main">
-      <p>{text}</p>
+      <h1 data-ns-test="project-name">{heading}</h1>
+      <p data-ns-test="project-description">{description}</p>
     </div>
   );
 };
